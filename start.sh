ec75474da1705ab35d50455bf7bbd04e5dcb9b10
rbourgea <rbourgea@42.fr>
2024-05-23 09:39:51 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+######################################################
+# Variables (you can edit)                           #
+######################################################
+
 # Stable Diffusion Model
 SD_MODEL=stabilityai/sdxl-turbo
 
+# TEST REMOVE THIS:
+MODEL=mistral-7b-instruct-v0.1.Q2_K.gguf
+MODEL_HF=TheBloke/Mistral-7B-Instruct-v0.1-GGUF:q2_k
+
 # English Mistral 7b Q4_K_M by default
-MODEL=mistral-7b-instruct-v0.1.Q4_K_M.gguf
-MODEL_HF=TheBloke/Mistral-7B-Instruct-v0.1-GGUF:q4_k_m
+# MODEL=mistral-7b-instruct-v0.1.Q4_K_M.gguf
+# MODEL_HF=TheBloke/Mistral-7B-Instruct-v0.1-GGUF:q4_k_m
 
 # French Mistral 7b Q4_K_M for example
 # MODEL=vigostral-7b-chat.Q4_K_M.gguf
@@ -13,24 +21,31 @@ MODEL_HF=TheBloke/Mistral-7B-Instruct-v0.1-GGUF:q4_k_m
 
 MODEL_PATH=models/
 
-MODEL_HF_UNDERSCORE=$(echo "$(echo "$MODEL_HF" | sed 's/\//_/g')" | cut -d':' -f1)
-RESULT_PATH="${MODEL_PATH}${MODEL_HF_UNDERSCORE}/${MODEL}"
+######################################################
+# Setup project                                      #
+######################################################
+
+declare -a PIDS
 
 cleanup() {
   echo "Stopping the app..."
-  kill -- -$$
-  pkill -f "node"
+  for pid in "${PIDS[@]}"; do
+    kill "$pid" 2>/dev/null
+  done
   exit 0
 }
 
 trap cleanup INT
 
-git submodule update --init --recursive --remote
+# git submodule update --init --recursive --remote
 
 # Install MODEL_HF
 bash <(curl -sSL https://g.bodaay.io/hfd) -m $MODEL_HF -s $MODEL_PATH
 
-# Check if Miniconda is installed
+######################################################
+# Python ENV setup                                   #
+######################################################
+
 if ! command -v conda &> /dev/null
 then
     # Download the latest Miniconda3 installer for Mac or Linux
@@ -51,24 +66,49 @@ then
     source ~/.bashrc
 fi
 
-# Create a new environment with Python 3.11
 conda create -n ImpAI python=3.11 -y
-
-# Activate the environment
 source activate ImpAI
 
-# Start backend (for image generation)
-cd backend && python3.11 -m pip install -r requirements.txt && python3.11 main.py $SD_MODEL &
+######################################################
+# Backend                                            #
+######################################################
 
-# Wait for the Flask server to be up before proceeding
+cd backend
+pip install -r requirements.txt
+python main.py "$SD_MODEL" &
+PIDS+=($!)
+
+echo "Waiting for backend to be up..."
 while ! curl -s http://127.0.0.1:7543 > /dev/null; do
     sleep 1
 done
+echo "Backend up !"
 
-# Start frontend
-cd frontend && npm i && npm start &
+######################################################
+# llama.cpp server                                   #
+######################################################
 
-# Start llama.cpp server
-cd llama.cpp && make && ./server -m ../$RESULT_PATH -c 32000 --port 7542 &
+MODEL_HF_UNDERSCORE=$(echo "$(echo "$MODEL_HF" | sed 's/\//_/g')" | cut -d':' -f1)
+RESULT_PATH="${MODEL_PATH}${MODEL_HF_UNDERSCORE}/${MODEL}"
+
+cd ../llama.cpp
+make
+./server -m "../$RESULT_PATH" -c 32000 --port 7542 &
+PIDS+=($!)
+
+echo "Waiting for llama.cpp server to be up..."
+while ! curl -s http://127.0.0.1:7542 > /dev/null; do
+    sleep 1
+done
+echo "llama.cpp up !"
+
+######################################################
+# Frontend                                           #
+######################################################
+
+cd ../frontend
+npm install
+npm start &
+PIDS+=($!)
 
 wait
